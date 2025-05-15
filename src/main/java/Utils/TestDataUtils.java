e96f14567c01bdd69fa7acf85239e274fcb5b835
ShyamRepo <149216298+ShyamRepo@users.noreply.github.com>
2025-05-15 07:07:58 +0530

    screenshot

--- src/main/java/Utils/TestDataUtils.java
+++ src/main/java/Utils/TestDataUtils.java
@@ -1,7 +1,28 @@
 package Utils;
 
-public class TestDataUtils {
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+
+public class TestDataUtils extends Baseclass {
 
 	
+	public TestDataUtils() throws IOException {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+
 	public static final long waitfor=10;
+	
+	
+	public static void takeScreenshot() throws IOException {
+		File scrFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+		String currentDir = System.getProperty("user.dir");
+		FileUtils.copyFile(scrFile, new File(currentDir + "/screenshots/" + System.currentTimeMillis() + ".png"));
+	}
+
 }
